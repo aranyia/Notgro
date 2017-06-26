4d81370bbedbaf21878b7987690af8daa0bd482b
aaranyi <Andras.Aranyi@wup.hu>
2017-06-26 10:57:39 +0200

    Added DiscountEngineTest test cases

--- src/test/java/org/aranyia/notgro/service/DiscountEngineTest.java
+++ src/test/java/org/aranyia/notgro/service/DiscountEngineTest.java
@@ -5,7 +5,9 @@ import org.aranyia.notgro.model.discount.ValueDiscount;
 import org.aranyia.notgro.model.item.GroceryItem;
 import org.aranyia.notgro.model.item.Item;
 import org.aranyia.notgro.model.Bill;
+import org.aranyia.notgro.model.user.Affiliate;
 import org.aranyia.notgro.model.user.Employee;
+import org.aranyia.notgro.model.user.User;
 import org.aranyia.notgro.predicates.DiscountFilter;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,41 +26,103 @@ import static org.junit.Assert.assertThat;
 @RunWith(JUnit4.class)
 public class DiscountEngineTest {
 
-    private DiscountEngine discountProcessor;
+    private DiscountEngine discountEngine;
 
     private UserDiscountValidator userDiscountValidator;
 
+    private Bill bill;
+
     @Before
     public void init() {
-        List<Predicate<Item>> discountFilters = Arrays.asList(DiscountFilter.fromAsNegated(GroceryItem.class));
-        discountProcessor = new DefaultDiscountEngine(discountFilters);
+        final List<Predicate<Item>> discountFilters = Arrays.asList(DiscountFilter.fromAsNegated(GroceryItem.class));
+        discountEngine = new DefaultDiscountEngine(discountFilters);
 
         userDiscountValidator = DefaultUserDiscountValidator.newBuilder()
-                .withAffiliateDiscountRate(.1)
+                .withAffiliateDiscountRate(0.1)
                 .withEmployeeDiscountRate(0.3)
-                .withLoyalCustomerDiscountRate(.05)
+                .withLoyalCustomerDiscountRate(0.05)
                 .withValueDiscountStep(100.0)
                 .withValueDiscount(5.0)
                 .build();
+
+        final List<Item> items =
+                Arrays.asList(new Item(100), new Item(80), new Item(30), new GroceryItem(5.0));
+        bill = new Bill(items);
     }
 
     @Test
-    public void testDiscounts() {
-        List<Item> items = Arrays.asList(new Item(100), new Item(80), new Item(30), new GroceryItem(5.0));
-        Bill bill = new Bill(items);
-
-        Employee user = new Employee();
+    public void testDiscountsForAffiliate() {
+        //given
+        final Affiliate user = new Affiliate();
         user.setRegistrationDate(LocalDate.now().minusYears(2).minusDays(10));
 
+        //when
         userDiscountValidator.getPercentageDiscount(user);
 
         final PercentageDiscount percentageDiscount = userDiscountValidator.getPercentageDiscount(user);
-
         final ValueDiscount valueDiscount = userDiscountValidator.getValueDiscount(user);
 
-        Bill billOut = discountProcessor.applyDiscounts(bill, percentageDiscount, valueDiscount);
+        //then
+        final Bill actualBill = discountEngine.applyDiscounts(bill, percentageDiscount, valueDiscount);
 
-        assertThat(billOut, notNullValue());
-        assertThat(billOut.getSumTotal(), equalTo(147.0));
+        assertThat(actualBill, notNullValue());
+        assertThat(actualBill.getSumTotal(), equalTo(189.0));
+    }
+
+    @Test
+    public void testDiscountsForEmployee() {
+        //given
+        final Employee user = new Employee();
+        user.setRegistrationDate(LocalDate.now().minusYears(2).minusDays(10));
+
+        //when
+        userDiscountValidator.getPercentageDiscount(user);
+
+        final PercentageDiscount percentageDiscount = userDiscountValidator.getPercentageDiscount(user);
+        final ValueDiscount valueDiscount = userDiscountValidator.getValueDiscount(user);
+
+        //then
+        final Bill actualBill = discountEngine.applyDiscounts(bill, percentageDiscount, valueDiscount);
+
+        assertThat(actualBill, notNullValue());
+        assertThat(actualBill.getSumTotal(), equalTo(147.0));
+    }
+
+    @Test
+    public void testDiscountsForLoyalCustomer() {
+        //given
+        final User user = new User();
+        user.setRegistrationDate(LocalDate.now().minusYears(2).minusDays(10));
+
+        //when
+        userDiscountValidator.getPercentageDiscount(user);
+
+        final PercentageDiscount percentageDiscount = userDiscountValidator.getPercentageDiscount(user);
+        final ValueDiscount valueDiscount = userDiscountValidator.getValueDiscount(user);
+
+        //then
+        final Bill actualBill = discountEngine.applyDiscounts(bill, percentageDiscount, valueDiscount);
+
+        assertThat(actualBill, notNullValue());
+        assertThat(actualBill.getSumTotal(), equalTo(194.5));
+    }
+
+    @Test
+    public void testDiscountsForCommonUser() {
+        //given
+        final User user = new User();
+        user.setRegistrationDate(LocalDate.now().minusDays(10));
+
+        //when
+        userDiscountValidator.getPercentageDiscount(user);
+
+        final PercentageDiscount percentageDiscount = userDiscountValidator.getPercentageDiscount(user);
+        final ValueDiscount valueDiscount = userDiscountValidator.getValueDiscount(user);
+
+        //then
+        final Bill actualBill = discountEngine.applyDiscounts(bill, percentageDiscount, valueDiscount);
+
+        assertThat(actualBill, notNullValue());
+        assertThat(actualBill.getSumTotal(), equalTo(205.0));
     }
 }
